737c49d66e5b306548041d9fb84bb106daa48b55
Jeremiah <jeremiahmarcos27@outlook.com>
2021-11-24 10:51:14 -0600

    Completed assingment

--- lib/song.rb
+++ lib/song.rb
@@ -23,27 +23,30 @@ class Song
     end
 
     def self.genre_count
-        genre_hash = {}
-        @@genres.each do |genre|
-            if genre_hash.key?(genre)
-                genre_hash[genre] +=1
-            else
-                genre_hash[genre] = 1
-            end
-        end
-        genre_hash
+        @@genres.tally
+        # genre_hash = {}
+        # @@genres.each do |genre|
+        #     if genre_hash.key?(genre)
+        #         genre_hash[genre] +=1
+        #     else
+        #         genre_hash[genre] = 1
+        #     end
+        # end
+        # genre_hash
     end
 
     def self.artist_count 
-                artist_hash = {}
-        @@artists.each do |artist|
-            if artist_hash.key?(artist)
-                artist_hash[artist] +=1
-            else
-                artist_hash[artist] = 1
-            end
-        end
-        artist_hash
+        @@artists.tally
     end
+    #             artist_hash = {}
+    #     @@artists.each do |artist|
+    #         if artist_hash.key?(artist)
+    #             artist_hash[artist] +=1
+    #         else
+    #             artist_hash[artist] = 1
+    #         end
+    #     end
+    #     artist_hash
+    # end
 
 end
